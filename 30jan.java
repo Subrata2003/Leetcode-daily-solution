db3e6de4bbdc7fadf75fc12077f21d431f954ed8
TithiB-del <76741863+TithiB-del@users.noreply.github.com>
2023-01-30 23:23:34 +0530

    1137. N-th Tribonacci Number

---
+++ 30jan.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int tribonacci(int n) {
+        int[] dp = new int[Math.max(n + 1, 3)];
+        dp[1] = dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+}
