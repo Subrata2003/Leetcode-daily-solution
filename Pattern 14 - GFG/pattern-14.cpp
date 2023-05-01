cd9e4703af9e1de3059d3391a25748f33e61860d
Subrata Bhuin <subrata.cse095@bppimt.ac.in>
2023-05-01 12:52:50 +0530

    Added solution - LeetHub

---
+++ Pattern 14 - GFG/pattern-14.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void printTriangle(int n) {
+        // code here
+        for ( int i=0; i<n; i++) {
+            for ( char ch='A'; ch<='A'+i; ch++) {
+                cout<<ch;
+            }
+        cout<<endl;
+            }
+            
+        }
+    
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        ob.printTriangle(n);
+    }
+    return 0;
+}
+// } Driver Code Ends
